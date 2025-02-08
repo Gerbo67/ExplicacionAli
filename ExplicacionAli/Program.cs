c5016054cdf928b48b5ac7c50b23b71093427d98
José Fernando Resendiz López <gerbo67@outlook.es>
2025-02-08 15:56:59 -0600

    Ejercicios 1-5

--- ExplicacionAli/Program.cs
+++ ExplicacionAli/Program.cs
@@ -1,16 +1,42 @@
-﻿using System;
-
-namespace ExplicacionAli;
+﻿namespace ExplicacionAli;
 
 class Program
 {
     private static void Main(string[] args)
     {
-        // Solicita al usuario que ingrese un texto
-        Console.Write("Ingrese un texto: ");
-        string? entrada = Console.ReadLine();
-
-        // Imprime el texto ingresado por el usuario
-        Console.WriteLine("Usted ingresó: " + entrada);
+        // Ejercicio 1
+        //Ejercicios.Ejercicio1();
+        
+        // Ejercicio 2
+        //Ejercicios.Ejercicio2();
+        
+        // Ejercicio 3
+        //Ejercicios.Ejercicio3();
+        
+        // Ejercicio 4
+        //Ejercicios.Ejercicio4();
+        
+        // Ejercicio 5
+        Ejercicios.Ejercicio5();
+        
+        
+        // Array de una dimension
+        // [2] = [1, 1]
+        // Columna 1, 1
+        
+        // Array de dos dimension = matriz
+        // [2][2] = [[1, 1], [1, 1]]
+        
+        // Columna 1, 1
+        // Columna 2, 2
+        
+        // Matriz de 3 dimensiones
+        // [2][2][2] = [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]
+        
+        // Matriz de 4 dimensiones
+        // [2][2][2][2] = [[[[1, 1], [1, 1]], [[1, 1], [1, 1]]], [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]]
+        
+        // Matriz de 5 dimensiones
+        // [3][4][1][6][10] = [[[[[1, 1], [1, 1]], [[1, 1], [1, 1]]], [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]], [[[[1, 1], [1, 1]], [[1, 1], [1, 1]]], [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]]]
     }
 }
