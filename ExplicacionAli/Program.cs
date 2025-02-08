492b2a5931b0420f640c18d490749a80563f17ef
José Fernando Resendiz López <gerbo67@outlook.es>
2025-02-08 12:07:21 -0600

    Initial commit

---
+++ ExplicacionAli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExplicacionAli;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        // Solicita al usuario que ingrese un texto
+        Console.Write("Ingrese un texto: ");
+        string? entrada = Console.ReadLine();
+
+        // Imprime el texto ingresado por el usuario
+        Console.WriteLine("Usted ingresó: " + entrada);
+    }
+}
